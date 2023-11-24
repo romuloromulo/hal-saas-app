ad046f3a20117277f8d4b106d5e5b177b5967fd3
Rômulo Viana <neoromulo@gmail.comgit config --global user.email neoromulo@gmail.comclgit config --global user.name Rmulo>
2023-11-23 23:29:47 -0300

    desenvolvimento

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
+
+function page() {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+
+  trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+    onError: (err) => {
+      if (err.data?.code === "UNAUTHORIZED") {
+        router.push("/sign-in");
+      }
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Configurando sua conta...</h3>
+        <p>Você será redirecionado automaticamente.</p>
+      </div>
+    </div>
+  );
+}
+
+export default page;
